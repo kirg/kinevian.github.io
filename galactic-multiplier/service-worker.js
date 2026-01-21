3a470b25b2b0b9b021e7eb69a782fbb9372e703b
Kiran RG <rgkiran@gmail.com>
2026-01-21 22:09:55 +0100

    galactic-multiplier v0.2

--- galactic-multiplier/service-worker.js
+++ galactic-multiplier/service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'galactic-math-v1';
 const ASSETS = [
   './index.html',
   './manifest.json',
-  '[https://cdn.tailwindcss.com](https://cdn.tailwindcss.com)',
-  '[https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js](https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js)',
-  '[https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;600&family=Press+Start+2P&display=swap](https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;600&family=Press+Start+2P&display=swap)'
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js',
+  'https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;600&family=Press+Start+2P&display=swap'
 ];
 
 self.addEventListener('install', (e) => {
